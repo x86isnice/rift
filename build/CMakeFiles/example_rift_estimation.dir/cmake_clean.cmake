d06e7e98b2ab104eef8718c9cce88c74af890d30
x86isnice <x86isnice@gmail.com>
2016-04-26 15:21:28 +0800

    changes log

---
+++ build/CMakeFiles/example_rift_estimation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_rift_estimation.dir/example_rift_estimation.cpp.o"
+  "example_rift_estimation.pdb"
+  "example_rift_estimation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_rift_estimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
